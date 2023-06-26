12a01675375286a47f74be7b3b8d857bccb7b1ac
n9dmitry <121239886+n9dmitry@users.noreply.github.com>
2023-06-26 12:51:39 +0300

    0.06 ads url

--- Invest/Invest/urls.py
+++ Invest/Invest/urls.py
@@ -20,4 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('registration', include('reg.urls')),
+    path('ads', include('ads.urls')),
+
 ]
--- Invest/ads/views.py
+++ Invest/ads/views.py
@@ -1,13 +1,17 @@
-
 from django.shortcuts import render, redirect
 from .forms import AddsForm
 
-def create_ad(request):
-    if request.method == 'POST':
-        form = AddsForm(request.POST, request.FILES)
-        if form.is_valid():
-            form.save()
-            return redirect('ads_list')
-    else:
-        form = AddsForm()
-    return render(request, 'create_ad.html', {'form': form})
+from django.shortcuts import render
+
+def ads(request):
+    return render(request, 'ads.html')
+
+# def create_ad(request):
+#     if request.method == 'POST':
+#         form = AddsForm(request.POST, request.FILES)
+#         if form.is_valid():
+#             form.save()
+#             return redirect('ads_list')
+#     else:
+#         form = AddsForm()
+#     return render(request, 'create_ad.html', {'form': form})
