11e70fe02410abb91262c3f19cc0b530c9765437
Trackktoor <pipleson212@gmail.com>
2023-07-04 16:13:42 +0500

    Стили для хедера

--- Invest/Invest/settings.py
+++ Invest/Invest/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'Invest',
         'USER': 'postgres',
-        'PASSWORD': 'Access777',
+        'PASSWORD': '666666',
         'HOST': 'localhost',
         'PORT': '5432',
     }
